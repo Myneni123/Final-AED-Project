38ad2bc2af77ea9c73f085a778728c8968e15524
namrathapilli05 <pilli.n@northeastern.edu>
2022-11-30 07:43:22 -0500

    defining a organization class

---
+++ AED Final Project/src/bussiness_organization/Organisation.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bussiness_organization;
+
+/**
+ *
+ * @author namra
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import Business.Employee.EmployeeDirectory;
+import Business.Role.Role;
+import Business.UserAccount.UserAccountDirectory;
+import Business.WorkQueue.WorkQueue;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author raunak
+ */
+public abstract class Organization {
+
+    private String name;
+    private WorkQueue workQueue;
+    private EmployeeDirectory employeeDirectory;
+    private UserAccountDirectory userAccountDirectory;
+    private int organizationID;
+    private static int counter=0;
+    public HashSet<Role> roles;
+    
+    public enum Type{
+        CDCManager("CDCManager Organization"),Clinic("Clinic Organization"),Supplier("Supplier Organization"), Provider("Provider Organization");
+        private String value;
+        private Type(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public Organization(String name) {
+        this.name = name;
+        workQueue = new WorkQueue();
+        employeeDirectory = new EmployeeDirectory();
+        userAccountDirectory = new UserAccountDirectory();
+        organizationID = counter;
+        roles = new HashSet<>();
+        ++counter;
+    }
+
+    public abstract HashSet<Role> getSupportedRole();
+    
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }
+
+    public int getOrganizationID() {
+        return organizationID;
+    }
+
+    public EmployeeDirectory getEmployeeDirectory() {
+        return employeeDirectory;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+}
+
