d5ff197527449dbdcac2e9c218eddcae29091d92
namrathapilli05 <pilli.n@northeastern.edu>
2022-12-05 00:07:35 -0500

    updating employee class

---
+++ AED Final Project/src/Business_Frame/employee.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business_Frame;
+
+/**
+ *
+ * @author namra
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Employee;
+
+/**
+ *
+ * @author raunak
+ */
+public class Employee {
+    
+    private String name;
+    private int id;
+    private static int count = 1;
+
+    public Employee() {
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+}
+
+
