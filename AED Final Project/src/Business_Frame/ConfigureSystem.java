c335f19aea0dfa5d584c045b96ed0eff6b6bbb06
mynenidivya <mynenidivya@mynenis-mbp.hsd1.ma.comcast.net>
2022-12-06 00:35:56 -0500

    Configuring the business frame

--- AED Final Project/src/Business_Frame/ConfigureSystem.java
+++ AED Final Project/src/Business_Frame/ConfigureSystem.java
@@ -4,10 +4,34 @@
  */
 package Business_Frame;
 
+import Business_Frame.EmployeeArea.Employee;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+
 /**
  *
  * @author mynenidivya
  */
 public class ConfigureSystem {
     
+    public static MainSystem configure(){
+        
+        MainSystem system = MainSystem.getInstance();
+        
+        //Create a networkFrame
+        //create an enterprise with organizations
+        //initialize some organizations with employees
+        //have some employees list
+        //create user account
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("admin", "admin", employee, new SystemAdminRole());
+        
+        return system;
+    }
+    
 }
+    
+
--- AED Final Project/src/Business_Frame/MainSystem.java
+++ AED Final Project/src/Business_Frame/MainSystem.java
@@ -4,10 +4,101 @@
  */
 package Business_Frame;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  *
  * @author mynenidivya
  */
-public class MainSystem {
+public class MainSystem extends OrganizationFrame {
+     private static MainSystem business;
+    private ArrayList<Network> networkList;
+    private VaccineDirectory vaccineList;
+    private DiseaseDirectory diseaseList;
+
+    public static MainSystem getInstance() {
+        if (business == null) {
+            business = new MainSystem();
+        }
+        return business;
+    }
+
+    private MainSystem() {
+        super(null);
+        networkList = new ArrayList<>();
+       vaccineList= new VaccineDirectory();
+       diseaseList= new DiseaseDirectory();
+    }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public VaccineDirectory getVaccineList() {
+        return vaccineList;
+    }
+
+    public void setVaccineList(VaccineDirectory vaccineList) {
+        this.vaccineList = vaccineList;
+    }
     
+    public static void setInstance(MainSystem system) {
+        business=system;
+    }
+    
+    public DiseaseDirectory getDiseaseList() {
+        return diseaseList;
+    }
+    
+    
+    public void setDiseaseList(DiseaseDirectory diseaseList) {
+        this.diseaseList = diseaseList;
+    }
+
+    public Network createAndAddNetwork() {
+        Network network = new Network();
+        networkList.add(network);
+        return network;
+    }
+
+    @Override
+    public HashSet<Role> getSupportedRole() {
+        roles.add(new SystemAdminRole());
+        roles.add(new HospitalAdminRole());
+        roles.add(new DistributorAdminRole());
+        roles.add(new CDCAdminRole());
+        return roles;
+    }
+
+    public static boolean checkIfUsernameIsUnique(String username) {
+
+      //  if (!this.getUserAccountDirectory().checkIfUsernameIsUnique(username)) {
+             for (Network network : business.getNetworkList()) {
+                 
+                for (Enterprise enterprise : network.getEnterpriseDirectory().getEnterpriseList()) {
+                    for (UserAccount ua : enterprise.getUserAccountDirectory().getUserAccountList()) {
+                        if(ua.getUsername().equals(username)){
+                            return false;
+                        }
+                    }
+                    
+                        for (Organization organization : enterprise.getOrganizationDirectory().getOrganizationList()) {
+                            for (UserAccount ua : organization.getUserAccountDirectory().getUserAccountList()) {
+                                 if(ua.getUsername().equals(username)){
+                            return false;
+                        }
+                    }
+                            }
+                        }
+                    }
+            
+      //  }
+
+       
+
+        return true;
+    }
 }
+    
+
