6408178007ea52f3f0561312ecaca5ca48e49cef
mynenidivya <mynenidivya@mynenis-mbp.hsd1.ma.comcast.net>
2022-12-06 20:41:58 -0500

    Initializing configuring the NetworkArea

---
+++ AED Final Project/src/Business/NetworkArea/NetworkArea.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.NetworkArea;
+
+import Business.Enterprise.EnterpriseDirectory;
+import Business.Organization.Organization;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mynenidivya
+ */
+public class NetworkArea {
+    
+    private String name;
+    private EnterpriseDirectory enterpriseDirectory;
+
+    public Network() {
+       
+        enterpriseDirectory = new EnterpriseDirectory();
+    }
+    
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
