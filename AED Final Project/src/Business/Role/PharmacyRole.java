c94cd2ecf58cd6c1230ef83119ff08e876863167
mynenidivya <mynenidivya@mynenis-mbp.hsd1.ma.comcast.net>
2022-12-07 23:56:42 -0500

    initializing the UI_Main frames and packages

--- AED Final Project/src/Business/Role/PharmacyRole.java
+++ AED Final Project/src/Business/Role/PharmacyRole.java
@@ -4,10 +4,21 @@
  */
 package Business.Role;
 
+import javax.swing.JPanel;
+
 /**
  *
  * @author mynenidivya
  */
 public class PharmacyRole {
+    
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+      return new PharmacyAdminWorkAreaJPanel(userProcessContainer,  account, (ClinicOrganization) organization,  enterprise,  business);
+    }
+     @Override
+    public String toString(){
+        return RoleType.PharmaManager.getValue();
+    }
     
 }
