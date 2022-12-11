961568f8ce4bed2f605842c7b3fcbddd157baddd
namrathapilli05 <pilli.n@northeastern.edu>
2022-12-10 20:36:34 -0500

    configuring the UI for DisturbutorManageOrg panel

--- AED Final Project/src/UI_Main/DistributorsArea/DistributorManageOrganization.java
+++ AED Final Project/src/UI_Main/DistributorsArea/DistributorManageOrganization.java
@@ -63,12 +63,17 @@ public class DistributorManageOrganization extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         Org_JTable = new javax.swing.JTable();
         add_OrgJButton = new javax.swing.JButton();
         OrgTypeJComboBox = new javax.swing.JComboBox();
         lblOrg = new javax.swing.JLabel();
         back_JButton = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\namra\\Downloads\\aed5.jpg")); // NOI18N
 
         setBackground(new java.awt.Color(255, 204, 204));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -105,7 +110,7 @@ public class DistributorManageOrganization extends javax.swing.JPanel {
             Org_JTable.getColumnModel().getColumn(1).setResizable(false);
         }
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(111, 60, 480, 92));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 340, 480, 92));
 
         add_OrgJButton.setText("Add Organization");
         add_OrgJButton.addActionListener(new java.awt.event.ActionListener() {
@@ -113,21 +118,25 @@ public class DistributorManageOrganization extends javax.swing.JPanel {
                 add_OrgJButtonActionPerformed(evt);
             }
         });
-        add(add_OrgJButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(469, 310, -1, -1));
+        add(add_OrgJButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 460, -1, -1));
 
         OrgTypeJComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        add(OrgTypeJComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 225, 81, -1));
+        add(OrgTypeJComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 220, 81, -1));
 
-        lblOrg.setText("Organization Type ");
-        add(lblOrg, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 228, -1, -1));
+        lblOrg.setText("Organization Category ");
+        add(lblOrg, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 210, -1, 40));
 
-        back_JButton.setText("<< Back");
+        back_JButton.setText(" Back");
         back_JButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 back_JButtonActionPerformed(evt);
             }
         });
-        add(back_JButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(66, 310, -1, -1));
+        add(back_JButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 20, -1, 110));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\namra\\Downloads\\aed7.png")); // NOI18N
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 880, 520));
     }// </editor-fold>//GEN-END:initComponents
 
     private void add_OrgJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_add_OrgJButtonActionPerformed
@@ -149,6 +158,9 @@ public class DistributorManageOrganization extends javax.swing.JPanel {
     private javax.swing.JTable Org_JTable;
     private javax.swing.JButton add_OrgJButton;
     private javax.swing.JButton back_JButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblOrg;
     // End of variables declaration//GEN-END:variables
